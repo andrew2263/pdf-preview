f91711d7b4d184a0b845e23d1d8d8d61173f954d
andrew2263 <andrew2263@gmail.com>
2023-08-28 23:45:11 +0300

    preview

---
+++ src/Components/PdfPreview.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const PdfPreview = ({ pdfUrl }) => {
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div className="pdf-preview">
+      <Document
+        file={pdfUrl}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{ workerSrc: '/pdf.worker.js' }} // Путь к pdf.worker.js
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    </div>
+  );
+};
+
+export default PdfPreview;
